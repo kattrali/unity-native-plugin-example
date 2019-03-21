36259c2dbfac18093b98aebe568943c1f9d8ff9c
Delisa Mason <iskanamagus@gmail.com>
2019-03-21 13:27:54 +0000

    Add example project using the package

---
+++ example/Assets/Checkers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Checkers {
+  public class Game {
+    private IntPtr game;
+    public Game(string playerName1, string playerName2) {
+      game = NativeInterface.game_init(playerName1, playerName2);
+    }
+
+    public bool MovePiece(int fromX, int fromY, int toX, int toY) {
+      var checker = NativeInterface.game_find_piece(game, fromX, fromY);
+      if (checker == null || checker == IntPtr.Zero) {
+        return false;
+      }
+      return NativeInterface.game_move_piece(game, checker, toX, toY);
+    }
+
+    public int NumberOfPiecesInPlay() {
+      return NativeInterface.game_pieces_in_play(game);
+    }
+
+    public int NumberOfPiecesInPlay(int playerIndex) {
+      return NativeInterface.game_pieces_for_player(game, playerIndex);
+    }
+
+    ~Game() {
+      NativeInterface.game_free(game);
+    }
+  }
+
+  internal class NativeInterface {
+
+    [DllImport("__Internal")]
+    public static extern IntPtr game_init(string playerName1, string playerName2);
+
+    [DllImport("__Internal")]
+    public static extern void game_free(IntPtr game);
+
+    [DllImport("__Internal")]
+    public static extern IntPtr game_find_piece(IntPtr game, int x, int y);
+
+    [DllImport("__Internal")]
+    public static extern bool game_move_piece(IntPtr game, IntPtr checker, int x, int y);
+
+    [DllImport("__Internal")]
+    public static extern int game_pieces_in_play(IntPtr game);
+
+    [DllImport("__Internal")]
+    public static extern int game_pieces_for_player(IntPtr game, int playerIndex);
+  }
+}
---
+++ example/Assets/CheckersGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Checkers;
+
+public class CheckersGame : MonoBehaviour {
+
+  private Game game;
+
+	void Start () {
+    game = new Game("sue", "bob");
+
+    Debug.Log("Number of pieces in play: " + game.NumberOfPiecesInPlay());
+
+    // (x, y) starts at the top left corner
+    bool success = game.MovePiece(1, 2, 2, 3);
+    Debug.Log("Did the move succeed? " + success);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
